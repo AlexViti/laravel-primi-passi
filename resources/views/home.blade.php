5958b3257caffa76de2fdc1ef704892114d97123
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-04-26 14:04:01 +0200

    Add dynamic text display

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel first steps</title>
+    <title>{{ $title }}</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1>{{ $title }}</h1>
+    <p>{{ $content }}</p>
 </body>
 </html>
